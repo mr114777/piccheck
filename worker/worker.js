e81f293da4d346131bc4d0159317fd1209363311
mr114777 <mr114777@users.noreply.github.com>
2026-03-06 05:57:40 +0900

    fix: add PATCH to CORS allowed methods, await cloud session load

--- worker/worker.js
+++ worker/worker.js
@@ -18,7 +18,7 @@ function generateId(len = 8) {
 // CORS headers
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 };
 
